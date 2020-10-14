d6f9259e540c181fd60910a5d5b0a125ce3941fd
OliKue# <oliver-kuetemeier123@web.de>
2020-10-14 17:38:57 +0200

    add List, LitItem, BasicInput Component

---
+++ webapp/src/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="list">
+      <ListItem v-for="item in listItems" :key="item.id" :item="item">
+    </ListItem>
+  </div>
+</template>
+
+
+<script>
+import  ListItem from './ListItem'  
+
+export default {
+  name: 'List',
+  components: {
+      ListItem
+  },
+  props: [
+      'listItems'
+  ]
+
+  
+}
+</script>
+
+<style>
+
+</style>
