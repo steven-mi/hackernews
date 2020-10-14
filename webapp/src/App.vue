735cdac5737b774f2272281e84e3518bc3cac636
OliKue# <oliver-kuetemeier123@web.de>
2020-10-14 17:38:57 +0200

    add List, LitItem, BasicInput Component

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>News List</h1>
+    <List :listItems='news'></List>
+    <BasicInput v-on:create-item='createItem($event)'></BasicInput>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import  List  from './components/List' 
+import  BasicInput  from './components/BasicInput' 
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{
+    List,
+    BasicInput
+  },
+  data: function () {
+    return{ news:[
+      {
+        id: 0,
+        title: 'post 1',
+        votes: 0
+      },
+      {
+        id: 1,
+        title: 'post 2',
+        votes: 0
+      },
+      {
+        id: 2,
+        title: 'post 3',
+        votes: 1
+      }
+    ]
+    }  
+  },
+  methods: {
+    createItem: function(newTitle){
+      this.news.push({
+          id: this.news.length,
+          title: newTitle,
+          votes: 0
+      })
+    }
+  },
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ webapp/src/components/ListItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="list-item">
+    
+    <h2>{{item.title}} - ({{item.votes}})</h2>
+    
+    <br />
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'ListItem',
+  props: [
+      'item'
+  ]
+}
+</script>
+
+<style>
+
+</style>
