528f15305e5572341e7c1d27224574329c18df5d
abteilung6 <moeller.tilman@gmail.com>
2020-10-21 17:55:04 +0200

    add upvote, downvote, remove, sort of items

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <h1>News List</h1>
-    <List :listItems='news'></List>
+    <List 
+      :listItems='news'
+      v-on:delete-item='deleteItem($event)'
+      v-on:update-item='updateItem($event)'
+    ></List>
     <BasicInput v-on:create-item='createItem($event)'></BasicInput>
   </div>
 </template>
@@ -18,33 +22,54 @@ export default {
     BasicInput
   },
   data: function () {
-    return{ news:[
-      {
-        id: 0,
-        title: 'post 1',
-        votes: 0
-      },
-      {
-        id: 1,
-        title: 'post 2',
-        votes: 0
-      },
-      {
-        id: 2,
-        title: 'post 3',
-        votes: 1
-      }
-    ]
+    return {
+      counter: 3,
+      news:[
+        {
+          id: 0,
+          title: 'post 1',
+          votes: 0
+        },
+        {
+          id: 1,
+          title: 'post 2',
+          votes: 0
+        },
+        {
+          id: 2,
+          title: 'post 3',
+          votes: 1
+        }
+      ],
     }  
   },
   methods: {
-    createItem: function(newTitle){
+    createItem: function(newTitle) {
       this.news.push({
-          id: this.news.length,
+          id: this.counter++,
           title: newTitle,
           votes: 0
+      });
+      this.sortItems();
+    },
+    deleteItem: function(item) {
+      this.news = this.news.filter(el => el.id != item.id);
+    },
+    updateItem: function(item) {
+      this.news.map(el => {
+        if (el.id == item.id) {
+          return item
+        }
+        return el
       })
+      this.sortItems();
+    },
+    sortItems: function() {
+      this.news = [...this.news].sort((a,b) => (a.votes < b.votes) ? 1 : ((b.votes < a.votes) ? -1 : 0));
     }
+  },
+  mounted: function() {
+    this.sortItems()
   },
   
 }
--- webapp/src/components/ListItem.vue
+++ webapp/src/components/ListItem.vue
@@ -2,7 +2,9 @@
   <div id="list-item">
     
     <h2>{{item.title}} - ({{item.votes}})</h2>
-    
+    <button @click="upvoteItem();$emit('update-item', item)">Upvote</button>
+    <button @click="downvoteItem();$emit('update-item', item)">Downvote</button>
+    <button @click="$emit('delete-item', item)">Remove</button>
     <br />
   </div>
 </template>
@@ -13,7 +15,15 @@ export default {
   name: 'ListItem',
   props: [
       'item'
-  ]
+  ],
+  methods: {
+    upvoteItem: function() {
+      this.item.votes++;
+    },
+    downvoteItem: function() {
+      this.item.votes--;
+    }
+  }
 }
 </script>
 
