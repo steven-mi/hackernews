a0ad686b0b67d47dd934f203717cc04fb12003f4
steven-mi <stevenmi@live.de>
2020-10-28 18:33:48 +0100

    Add linter, vue tests and dependencies, sort button and empty message

--- webapp/src/components/List.vue
+++ webapp/src/components/List.vue
@@ -1,30 +1,32 @@
 <template>
   <div id="list">
-      <ListItem 
-      v-for="item in listItems" 
-      :key="item.id" 
-      :item="item" 
-      v-on:delete-item="$emit('delete-item', $event)"
-      v-on:update-item="$emit('update-item', $event)"
-      >
+    <ListItem
+        v-for="item in listItems"
+        :key="item.id"
+        :item="item"
+        v-on:delete-item="$emit('delete-item', $event)"
+        v-on:update-item="$emit('update-item', $event)"
+    >
     </ListItem>
+    <p v-if="listItems.length === 0">The list is empty :(</p>
+    <button @click="$emit('sort-item')" id="button_sort">Sort</button>
   </div>
 </template>
 
 
 <script>
-import  ListItem from './ListItem'  
+import ListItem from './ListItem'
 
 export default {
   name: 'List',
   components: {
-      ListItem
+    ListItem
   },
   props: [
-      'listItems'
+    'listItems',
   ]
 
-  
+
 }
 </script>
 
