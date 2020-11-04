e2e1e5208d10d40eef7a8d31a36a58fcafaa678c
steven-mi <stevenmi@live.de>
2020-11-04 18:34:16 +0100

    add storybook for all component

---
+++ webapp/src/components/BasicInput/BasicInput.stories.js
@@ -0,0 +1,17 @@
+import BasicInput from './BasicInput.vue';
+
+export default {
+    title: 'Example/BasicInput',
+    component: BasicInput,
+};
+
+const Template = (args, {argTypes}) => ({
+    props: Object.keys(argTypes),
+    components: {BasicInput},
+    template:
+        '<BasicInput />',
+});
+
+export const standardBasicInput = Template.bind({});
+standardBasicInput.args = {};
+
