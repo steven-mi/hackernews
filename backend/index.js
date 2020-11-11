6ff4e5e424fa9ea025d99f0f0902872508bdf9b7
abteilung6 <moeller.tilman@gmail.com>
2020-11-11 16:43:11 +0100

    add post schema

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    title: String!
+    votes: Int!
+    author: User!
+  }
+
+  type User {
+    name: ID!
+    posts: [Post]
+  }
+
+  type Query {
+    posts: [Post]
+    users: [User]
+  }
+`;
