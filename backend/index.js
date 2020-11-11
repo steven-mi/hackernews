5fb50d71d8fcec0078ebc3e6174c9e08e11b31fe
abteilung6 <moeller.tilman@gmail.com>
2020-11-11 17:18:40 +0100

    add dataset, resolver and apolloserver

--- backend/index.js
+++ backend/index.js
@@ -21,3 +21,49 @@ const typeDefs = gql`
     users: [User]
   }
 `;
+
+const posts = [
+  {
+    id: "post0",
+    title: "post 1",
+    votes: 0,
+    author: "alice",
+  },
+  {
+    id: "post1",
+    title: "post 2",
+    votes: 0,
+    author: "alice",
+  },
+  {
+    id: "post2",
+    title: "post 3",
+    votes: 1,
+    author: "bob",
+  },
+];
+
+const users = [
+  {
+    name: "alice",
+    posts: ["post0", "post1"],
+  },
+  {
+    name: "bob",
+    posts: ["post2"],
+  },
+];
+
+const resolvers = {
+  Query: {
+    posts: () => posts,
+    users: () => users,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
