9027711510371cf8609f4ad40efd846709f6ed12
OliKue# <oliver-kuetemeier123@web.de>
2020-11-21 11:05:54 +0100

    refactor initialization of dummydata

--- backend/db.js
+++ backend/db.js
@@ -20,10 +20,12 @@ export class InMemoryDataSource extends DataSource {
         super()
         this.posts = []
         this.users = []
+        this.initDummyUsers()
+        this.initDummyPosts()
     }
 
     initialize(...args) {
-        // console.log(args)
+        //Warum wird diese Methode so oft ausgeführt?
     }
 
     createPost(data) {
@@ -42,4 +44,46 @@ export class InMemoryDataSource extends DataSource {
         return newUser
     }
 
+    // Init with dummydata
+    initDummyUsers() {
+        this.users = [
+            new User({
+                name: "Alice",
+                posts: ["post 1", "post 2"],
+                upvotes: []
+            }),
+            new User({
+                name: "Bob",
+                posts: ["post 3"],
+                upvotes: []
+            }),
+            new User({
+                name: "Hans",
+                posts: [],
+                upvotes: []
+            })
+        ];
+        console.log("Initialize 3 dummyusers.")
+    }
+
+    initDummyPosts() {
+        this.posts = [
+            new Post({
+                title: "post 1",
+                votes: [],
+                author: this.users[0],
+            }),
+            new Post({
+                title: "post 2",
+                votes: [],
+                author: this.users[0],
+            }),
+            new Post({
+                title: "post 3",
+                votes: [],
+                author: this.users[1],
+            })
+        ]
+        console.log("Initialize 3 dummypostss.")
+    }
 }
--- backend/server.js
+++ backend/server.js
@@ -3,49 +3,10 @@ import typeDefs from './typeDefs'
 import { Post, InMemoryDataSource } from './db'
 
 const db = new InMemoryDataSource()
-
-db.users = [{
-        name: "Alice",
-        posts: ["post 1", "post 2"],
-        upvotes: []
-    },
-    {
-        name: "Bob",
-        posts: ["post 3"],
-        upvotes: []
-    },
-    {
-        name: "Hans",
-        posts: [],
-        upvotes: []
-    }
-
-];
-
-db.posts = [
-    new Post({
-        title: "post 1",
-        votes: [],
-        author: db.users[0],
-    }),
-    new Post({
-        title: "post 2",
-        votes: [],
-        author: db.users[0],
-    }),
-    new Post({
-        title: "post 3",
-        votes: [],
-        author: db.users[1],
-    })
-]
+console.log("test")
 
 const dataSources = () => ({ db })
-
 const context = ({ req, res }) => ({ req, res })
-
-
-
 
 
 const resolvers = {
