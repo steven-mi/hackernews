a0fb95a53aa4c07760444d23c4421e1ed56c63de
steven-mi <stevenmi@live.de>
2020-12-02 17:58:14 +0100

    initialize user, db

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,51 @@
 import Server from './src/server'
-import {config} from 'dotenv'
+import dotenv from 'dotenv'
+import {InMemoryDataSource, Post, User} from "./src/db";
 
-config()
+const result = dotenv.config()
+if (result.error) {
+    console.log(".env not found")
+}
 
-const server = new Server()
-    // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
+
+const users = [
+    new User({
+        name: "Alice",
+        email: "alice@test.de",
+    }),
+    new User({
+        name: "Bob",
+        email: "bob@test.de",
+    }),
+];
+
+const posts = [
+    new Post({
+        title: "post 1",
+        author: users[0],
+    }),
+    new Post({
+        title: "post 2",
+        author: users[0],
+    }),
+    new Post({
+        title: "post 3",
+        author: users[1],
+    })
+]
+
+const token = process.env.TOKEN
+
+const playground = {
+    settings: {
+        'schema.polling.enable': false
+    }
+}
+const db = new InMemoryDataSource(posts, users, token)
+const server = new Server({
+    playground,
+    dataSources: () => ({db})
+})
+server.listen().then(({url}) => {
     console.log(`🚀  Server ready at ${url}`);
 });
