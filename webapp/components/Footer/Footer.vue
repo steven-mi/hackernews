63cdbd78c596a7479fb440911d8baa5c508bb91c
steven-mi <stevenmi@live.de>
2021-05-28 11:23:41 +0200

    change footer

--- webapp/components/Footer/Footer.vue
+++ webapp/components/Footer/Footer.vue
@@ -5,7 +5,7 @@
       <div class="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
         <div class="sm:w-2/3 text-center py-6">
           <p class="text-sm text-indigo-600 font-bold mb-2">
-            © 2021 by abteilung6
+            © 2021 by steven-mi
           </p>
         </div>
       </div>
